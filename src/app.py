13d27bd25b8c097c0d3fd86ea8d44f6f2facae28
vjabhi00985 <princeabhi00985@gmail.com>
2024-03-17 20:34:42 +0530

    Flask app updated

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "<h1>Hello, World!</h1>"
+	return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', debug=True)
